855c23ec2d062f9bab6e059a69388fa8c125ae0f
Olena <155203553+elgatoafk@users.noreply.github.com>
2024-05-12 13:28:26 +0300

    Fix uploading script
    Add missing loop to move through quotes list

--- upload_data.py
+++ upload_data.py
@@ -3,8 +3,7 @@ from mongoengine import connect
 from models import Authors, Quotes
 import json
 
-connect(host=f"""mongodb+srv://{MONGO_USER}:{MONGO_PASS}@{DOMAIN}/
-        {DB_NAME}?retryWrites=true&w=majority""", ssl=True)
+connect(host=f"""mongodb+srv://{MONGO_USER}:{MONGO_PASS}@{DOMAIN}/{DB_NAME}?retryWrites=true&w=majority""", ssl=True)
 
 
 def load_authors():
@@ -20,13 +19,13 @@ def load_authors():
 def load_quotes():
     with open("quotes.json", "r", encoding="utf-8") as f:
         quotes = json.load(f)
-        
-    author_name = quotes["author"]
-    author = Authors.objects(name=author_name).first()
-    if not author:
-        author = Authors(name=author_name, born_date="", born_location="", description="")
-        author.save()
-    Quotes(tags=quotes["tags"], author=author, quote=quotes["quote"]).save()
+    for quote in quotes:    
+        author_name = quote["author"]
+        author = Authors.objects(name=author_name).first()
+        if not author:
+            author = Authors(name=author_name, born_date="", born_location="", description="")
+            author.save()
+        Quotes(tags=quote["tags"], author=author, quote=quote["quote"]).save()
 
 
 if __name__ == "__main__":
